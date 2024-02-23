48c4afc59caa228fdf5f466076f29e2ba8d96106
balbo97 <balbosauro1@gmail.com>
2024-02-23 21:55:37 +0800

    migration completed

--- database/migrations/2024_02_23_114806_create_cars_table.php
+++ database/migrations/2024_02_23_114806_create_cars_table.php
@@ -15,8 +15,23 @@ return new class extends Migration
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
+            $table->string('model', 50);
+            $table->string('brand', 30);
+            $table->integer('year');
+            $table->string('type_of_engine');
+            $table->string('plate', 7)->nullable()->unique();
+            $table->string('type_of_gear', 30);
+            $table->string('n_chassis', 17)->nullable()->unique();
+            $table->float('price', 8, 2);
+            $table->integer('doors');
+            $table->integer('seats');
+            $table->string('color', 20);
+            $table->integer('power');
+            $table->string('photos', 255)->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
+        
     }
 
     /**
