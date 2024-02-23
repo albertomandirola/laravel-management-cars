ca222565848af47ae3b25ee3057fbcf9810cb21f
balbo97 <balbosauro1@gmail.com>
2024-02-23 22:43:43 +0800

    seeding completed

--- database/seeders/CarSeeder.php
+++ database/seeders/CarSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Faker\Generator as Faker;
+use App\Models\Car;
+
 class CarSeeder extends Seeder
 {
     /**
@@ -12,8 +15,28 @@ class CarSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 25; $i++) {
+            $new_car = new Car();
+
+            $new_car->model = $faker->word;
+            $new_car->brand = $faker->numberBetween(1, 10); 
+            $new_car->year = $faker->numberBetween(2000, 2022);
+            $new_car->type_of_engine = $faker->word;
+            $new_car->plate = $faker->optional()->numberBetween(1, 9999999); 
+            $new_car->type_of_gear = $faker->word;
+            $new_car->n_chassis = $faker->optional()->numberBetween(1, 99999999999999999);
+            $new_car->price = $faker->randomFloat(2, 1000, 50000);
+            $new_car->doors = $faker->numberBetween(2, 5);
+            $new_car->seats = $faker->numberBetween(2, 7);
+            $new_car->color = $faker->colorName;
+            $new_car->power = $faker->numberBetween(50, 500);
+            $new_car->photos = $faker->imageUrl();
+            $new_car->description = $faker->paragraph;
+
+            $new_car->save();
+        }
+
     }
 }
