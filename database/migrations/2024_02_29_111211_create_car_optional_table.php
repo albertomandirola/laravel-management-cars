ad00fcce90faeddac44162f91ef5c5fc29fab168
pietrostraneo <straneo_p@yahoo.it>
2024-02-29 12:42:40 +0100

    Add optional relationship to Car model
    Added a new optional relationship to the Car model for associating car options with specific cars, enhancing flexibility and customization. This will facilitate querying and managing optional features for each car. No issue references.

---
+++ database/migrations/2024_02_29_111211_create_car_optional_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car_optional', function (Blueprint $table) {
+            $table->unsignedBigInteger('car_id');
+            $table->foreign('car_id')->references('id')->on('cars');
+
+            $table->unsignedBigInteger('optional_id');
+            $table->foreign('optional_id')->references('id')->on('optionals');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropForeign('car_id');
+        Schema::dropForeign('optional_id');
+        Schema::dropIfExists('car_optional');
+    }
+};
